3e2b169da29b11326cc26c74962e40ec4b0d1a85
TrongDuc99 <ducphan.24hdev@gmail.com>
2021-01-21 09:59:02 +0700

    Fixed description in billing

--- backend/billing.js
+++ backend/billing.js
@@ -5,7 +5,7 @@ import { calculateCost } from "./libs/billing-lib";
 export const main = handler(async (event, context) => {
   const { storage, source } = JSON.parse(event.body);
   const amount = calculateCost(storage);
-  const description = "Scratch charge";
+  const description = "Notebook charge";
 
   // Load our secret key from the  environment variables
   const stripe = stripePackage(process.env.stripeSecretKey);
