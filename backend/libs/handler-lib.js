fa28cadc6dd2719f370c6fe04b9788dd9e6284de
TrongDuc99 <ducphan.24hdev@gmail.com>
2021-01-21 09:03:13 +0700

    Adding some faulty code

--- backend/libs/handler-lib.js
+++ backend/libs/handler-lib.js
@@ -1,4 +1,4 @@
-import * as debug from "./debug-lib";
+import * as debug from "../services/notes/libs/debug-lib";
 
 export default function handler(lambda) {
   return async function (event, context) {
