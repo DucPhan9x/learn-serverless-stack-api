d1900977b99c430d8c1ead72823f966f08ec3a84
TrongDuc99 <ducphan.24hdev@gmail.com>
2021-01-21 09:32:45 +0700

    Reverting faulty code

--- notes-app-client/src/containers/Home.js
+++ notes-app-client/src/containers/Home.js
@@ -32,7 +32,7 @@ export default function Home() {
   }, [isAuthenticated]);
 
   function loadNotes() {
-    return API.put("notes", "/notes");
+    return API.get("notes", "/notes");
   }
 
   function renderNotesList(notes) {
